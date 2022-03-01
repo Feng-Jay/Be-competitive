cfaeac9baf90c114a28503178489f29906fb5b5e
李丰杰 <ffengjay@192.168.124.3>
2022-03-02 06:57:01 +0800

    stream done

--- CS106L/Lecture3/3_streams/Istreams/istreams.cpp
+++ CS106L/Lecture3/3_streams/Istreams/istreams.cpp
@@ -8,47 +8,58 @@ using std::string; using std::cin;
 
 //read numbers from a file
 void readNumbers() {
+    std:: ifstream input("res/numbers.txt");
     // Create our ifstream and make it open the file
-    
-
+    int read;
     // Something to store values we get from the stream
-
+    while(true){
+        input >> read;
         // Extract next number from input
-        
-
+        if(input.fail() || input.eof()){
+            break;
+        }
         // If input is in a fail state, either a value couldn't
         // be converted, or we are at the end of the file.
-
+        cout<< read << endl;
         //print whatever we read
+    }
         
 }
 
 //read a single word at a time from a file
 void readHaikuWord() {
+    std::ifstream input("res/haiku.txt");
     // Create our ifstream and make it open the file
-
+    string temp;
      // Something to store values we get from the stream
-
+    while(true){
+        input >> temp;
         // Extract next word from input
-
-
+        if(input.fail() || input.eof()){
+            break;
+        }
         // If input is in a fail state, either a value couldn't
         // be converted, or we are at the end of the file.
-
+        cout<<temp<<endl;
         //print whatever we read
-
+    }
 }
 
 void readHaikuLine() {
+    std::ifstream input("res/haiku.txt");
     // Create our ifstream and make it open the file
-
+    string temp;
     // Something to store the lines as we get them form the stream
-
+    while(true){
+        std::getline(input, temp);
+        if(input.fail() || input.eof()){
+            break;
+        }
         // If input is in a fail state, either a value couldn't
         // be converted, or we are at the end of the file.
-
+        cout<<temp<<endl;
         //print whatever we read
-
+    }
 }
 
 
@@ -75,7 +86,13 @@ void playground(){
 
 int main() {
 
-    playground();
+    // playground();
+    cout<<"readNumbers"<<endl;
+    readNumbers();
+    cout<<"readHaikuWord"<<endl;
+    readHaikuWord();
+    cout<<"readHaikuLine"<<endl;
+    readHaikuLine();
 
     return 0;
 }
--- CS106L/Lecture3/3_streams/Ostreams/ostreams.cpp
+++ CS106L/Lecture3/3_streams/Ostreams/ostreams.cpp
@@ -10,10 +10,12 @@ using std::endl;
 using std::string;
 
 //function to write num to any ostream
-
+void wtite2anyostream(std::ostream &want_ostream, int num)
+{
+    want_ostream << "In the integrated function, The answer is: "<< num <<endl;
+}
 
 int main() {
-    std:: cin << ;
     // Write an int to the user's console.
     int myNum = 42;
     cout << "The answer is: " << myNum << endl;
@@ -23,8 +25,11 @@ int main() {
 
     out_stream << "The answer is: " << myNum << endl;
     
-
+    std::ofstream fileOut("out.txt");
     // Write method to take any ostream
+    wtite2anyostream(cout, myNum+1);
+    wtite2anyostream(fileOut, myNum+1);
+    // write over method.
     
    
     return 0;
