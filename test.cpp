d2eb49bc62a96114d90e21fd233d7146731faa20
ffengjay <2652925120@qq.com>
2022-03-23 10:19:16 +0800

    CHAPTER2 DONE!

--- test.cpp
+++ test.cpp
@@ -3,7 +3,18 @@ using namespace std;
 
 int main()
 {
-    short i = 1;
-    short* res = &i;
-    cout << res<<endl; 
+    // int mylist[2] = {1,2};
+    // cout<<&mylist[1] - &mylist[0]<<endl;
+    // cout << res<<endl; 
+    struct test
+    {
+        int i;
+        int j;
+        char m;
+    };
+    test temp={1,2,3};
+    cout<<&(temp.i)<<endl;
+    cout<<&(temp.m)<<endl;
+    cout<<&(temp.j)<<endl;
+    cout<<sizeof(temp)<<endl;
 }
